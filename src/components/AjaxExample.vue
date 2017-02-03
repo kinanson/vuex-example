a8b58be415e02ac8e6e332837b6f98766b0b6e58
kinanson <kinanson@gmail.com>
2017-02-03 21:18:03 +0800

    資料讀取完才進入頁面

--- src/components/AjaxExample.vue
+++ src/components/AjaxExample.vue
@@ -8,6 +8,8 @@
     mapActions
   } from 'vuex'
 
+  import store from '../store'
+
   export default {
     name: 'ajaxExample',
     methods: {
@@ -20,9 +22,13 @@
         'data'
       ])
     },
-    async created() {
-      await this.$store.dispatch('ajaxExample/get') //因為要取得預設資料，所以先去呼叫一次action，可用this去呼叫store
-      console.log(this.data) //這邊是getters去取得的data
+    // async created() {
+    //   await this.$store.dispatch('ajaxExample/get') //因為要取得預設資料，所以先去呼叫一次action，可用this去呼叫store
+    //   console.log(this.data) //這邊是getters去取得的data
+    // }
+    async beforeRouteEnter(to, from, next) {
+      await store.dispatch('ajaxExample/get') //因為要取得預設資料，所以先去呼叫一次action
+      next() //繼續執行
     }
   }
 
